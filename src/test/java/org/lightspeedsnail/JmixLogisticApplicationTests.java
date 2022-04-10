7c24e7e98eda895d23d4362021c22185cda8f2b5
jabberwocky <jabberwahh@gmail.com>
2022-04-10 19:26:52 +0300

    Initial commit

---
+++ src/test/java/org/lightspeedsnail/JmixLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lightspeedsnail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixLogisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
